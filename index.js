a767d99d2814315091b1385277a8c8d3931672ef
nurulislamrimon <nurulislamrimon@gmail.com>
2024-01-01 22:05:28 +0600

    index file created

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("hello");
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful!");
+  })
+  .catch((e) => console.log(`DB connection error: ${e}`));
+
+// request parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+// set public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+
+// error handling
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening at port: ${process.env.PORT}`);
+});
